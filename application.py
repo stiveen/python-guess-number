ccb9a72595d9e266b4c6539047069c5e323b796e
JuanCa97 <jjuanccarlos1997@hotmail.com>
2015-09-22 15:52:46 -0600

    todo documentado

--- application.py
+++ application.py
@@ -1,13 +1,12 @@
-# Aqui escribe tu codigo
+import random #Random number generation
+number=random.randrange(1, 20)#The game most generate random number from 1 to 20
+print number #print the number genetared
 
-print "Hello World"
-import random 
+ing = int(raw_input("insert a number in range 1 to 20: "))
 
-number=random.randrange(1, 20)
-
-ing = raw_input("insert a number in range 1 to 20")
-
-if number < ing :
-	print "the number is too small, please try again" 
-elif number >ing:
-	print "the number is too big, please try again"
+if ing > number: #if you insert a number that its higher that the generated number then i will recived the mesage you guess to high pleas try again
+   print "the number is too high, try again please" 
+elif ing < number: # if you insert a number that is lower that the generated number then i will recived the mesage you guess to lo please try again.
+    print "the number is too low, try again please"
+elif ing == number: #if you guess the number the game will give me the mesage "You Win"
+	print "YOU WIN"
