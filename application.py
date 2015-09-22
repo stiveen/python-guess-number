4b25a51621a89729de37e8bd057fd1973cba1cb3
JuanCa97 <jjuanccarlos1997@hotmail.com>
2015-09-22 15:20:58 -0600

    Documents Configured

--- application.py
+++ application.py
@@ -8,6 +8,6 @@ number=random.randrange(1, 20)
 ing = raw_input("insert a number in range 1 to 20")
 
 if number < ing :
-	print "the number is too small" 
+	print "the number is too small, please try again" 
 elif number >ing:
-	print "the number is too big"
+	print "the number is too big, please try again"
