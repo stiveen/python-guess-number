442a45e260e6dfbaca2ac1fb97b90d847bbb25af
JuanCa97 <jjuanccarlos1997@hotmail.com>
2015-09-22 13:04:55 -0600

    First modification

--- application.py
+++ application.py
@@ -1,3 +1,13 @@
 # Aqui escribe tu codigo
 
 print "Hello World"
+import random 
+
+number=random.randrange(1, 20)
+
+ing = raw_input("ingrese un numero")
+
+if number < ing:
+	print "el numero es demasiado pequeno" 
+elif number >ing:
+	print "el numero es demasaido grande
