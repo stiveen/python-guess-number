3daeeeea29fc681f6efc449769eb8112dbd8029e
edson1998 <muozperdomo1998.emo@gmail.com>
2015-09-23 10:08:45 -0600

    application modifiqued

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
+"""welcome to game guess the number"""
 import random #Random number generation
-number=random.randrange(1, 20)#The game most generate random number from 1 to 20
-print number #print the number genetared
 
-ing = int(raw_input("insert a number in range 1 to 20: "))
+INGRESAR = random.randrange(1, 20) #The game most generate random number from 1 to 20
+print INGRESAR #print the number
 
-if ing > number: #if you insert a number that its higher that the generated number then i will recived the mesage you guess to high pleas try again
-   print "the number is too high, try again please" 
-elif ing < number: # if you insert a number that is lower that the generated number then i will recived the mesage you guess to lo please try again.
-    print "the number is too low, try again please"
-elif ing == number: #if you guess the number the game will give me the mesage "You Win"
-	print "YOU WIN"
+GENERATE = int(raw_input("insert a number in range 1 to 20: "))
+
+if GENERATE < INGRESAR:  #if you insert a number that its higher that the generated number then i will recived the mesage you guess to high pleas try again
+    print"the number is too small, try again please"
+elif GENERATE > INGRESAR: # if you insert a number that is lower that the generated number then i will recived the mesage you guess to lo please try again.
+    print"the number is too big, try again please"
+elif GENERATE == INGRESAR: #if you guess the number the game will give me the mesage "You Win"
+    print "YOU WIN" #here print mesage to win
+
